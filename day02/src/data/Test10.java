d6a2ce131819fbc95684321cb8d69651fa5f0e5d
wlslals0805 <wlslals0805@naver.com>
2023-06-20 15:58:07 +0900

    실습

---
+++ day02/src/data/Test10.java
@@ -0,0 +1,43 @@
+package data;
+
+public class Test10 {
+		
+		public static void main(String[] args) {
+			
+//			int a=(60*2+45); 
+//			int b=(60+50);
+//			
+//			int c=a+b;
+//			int d=c/60;
+//			int e=c%60;
+//			
+//			System.out.println(c);
+//			System.out.println(d);
+//			System.out.println(e);
+			
+			//시간은 가장 작은 단위로 변환하여 푼다
+			
+			//입력
+			int yesterdayHour=2, yesterdayMin=45;
+			int todayHour=1, todayMin=50;
+			
+			//계산
+			
+		int yesterdayTime=yesterdayHour*60+yesterdayMin;
+		int todayTime=todayHour*60+todayMin;
+		
+		int time= yesterdayTime+todayTime;
+		
+		int hour=time/60;
+		int minute=time%60;
+		
+		//출력
+		
+		System.out.println(hour);
+		System.out.println(minute);
+		
+			
+		
+	}
+
+}
---
+++ day02/src/data/Test9.java
@@ -0,0 +1,21 @@
+package data;
+
+public class Test9 {
+		
+		public static void main(String[] args) {
+		
+			//입력
+			long money=20000000L;
+//			int b=12;
+			int year=30;
+			
+			//계산
+			int month=year*12;
+			long total=money*month;
+			
+			//출력
+			System.out.println(total);
+		
+	}
+
+}
