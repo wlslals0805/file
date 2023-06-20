0d1f8f5e96b8860d37ebe987e85ffb406a933937
wlslals0805 <wlslals0805@naver.com>
2023-06-20 14:27:06 +0900

    정수의 특징

---
+++ day02/src/data/Test8.java
@@ -0,0 +1,26 @@
+package data;
+
+public class Test8 {
+		
+		public static void main(String[] args) {
+			//정수의 특징
+			
+			//[1] 정수는 소수점이 없는 수이다.
+//			int a=3.14;
+			int a=3;
+			
+			//[2] 정수는 크기에 따라 여러 종류로 나뉜다.
+			//-byte, short, int, long
+			byte b=10;
+			
+			//[3] 크기를 벗어나면 저장이 안된다.
+//			byte c=128;
+//			int d=1000000000
+			long e=10000000000L;  //long은 표시를 해야한다.
+			System.out.println(e);
+			
+		
+		
+	}
+
+}
