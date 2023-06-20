b88847a40eb771f1300c46dcd14a38d411d46ecb
wlslals0805 <wlslals0805@naver.com>
2023-06-20 10:48:27 +0900

    2일차 프로젝트 생성2

---
+++ day02/src/day02/Hello.java
@@ -0,0 +1,12 @@
+package day02; //이 파일의 위치
+
+public class Hello {//Hello 클래스(=파일명에 따라서 결정됨)
+		public static void main(String[] args) {//main 메소드 (고정)
+			System.out.println("Hello!"); //출력 명령 
+			System.out.println(100); 
+	
+		}
+	
+	}
+
+
