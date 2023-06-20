78783fb05e399482fb7f04448be832820b221aba
wlslals0805 <wlslals0805@naver.com>
2023-06-20 11:15:51 +0900

    따옴표를 쓰는 기준

---
+++ day02/src/data/Test01.java
@@ -0,0 +1,15 @@
+package data;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		//따옴표를 쓰는 기준은 "계산"을 하는가이다.
+		System.out.println("2023-06-20");//글자 출력
+		System.out.println(2023-06-20);//숫자 출력
+	
+	}
+	
+	
+	
+	
+}
