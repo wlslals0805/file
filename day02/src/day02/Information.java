f326e3cfa5f9db1144be3fe9b6c24cf7df8a1c77
wlslals0805 <wlslals0805@naver.com>
2023-06-20 10:53:30 +0900

    연습

---
+++ day02/src/day02/Information.java
@@ -0,0 +1,11 @@
+package day02;
+
+public class Information {
+
+	public static void main(String[] args) {
+		System.out.println("디지털 컨버전스 공공테이터 융합 웹 애플리케이션 개발자 양성과정4");
+				System.out.println("황인빈 강사님");
+				System.out.println("박진희");
+	}
+
+}
