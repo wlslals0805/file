4b5cf4a5123dca5faae936ca6c5d8fe5419b8553
wlslals0805 <wlslals0805@naver.com>
2023-06-20 12:52:07 +0900

    변수를 최초로 사용

---
+++ day02/src/data/Test03.java
@@ -0,0 +1,17 @@
+package data;
+
+public class Test03 {
+		public static void main(String[] args) {
+			//변수(Variable)=데이터 기억장치
+
+			int a=7000*2; //7000 *2를 계산해서 a라는 이름으로 저장(대입)
+			int b=8000*3;//8000*3을 계산해서 b라는 이름으로 저장(대입)
+			
+			System.out.println(a); //a에 저장된 데이터를 출력 
+			System.out.println(b);//b에 저장된 데이터를 출력
+			System.out.println(a+b);
+ 		
+		}
+		}
+
+
