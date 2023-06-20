5f29f1f6e64e011e609dfba8f1188b828a4f2978
wlslals0805 <wlslals0805@naver.com>
2023-06-20 14:50:49 +0900

    정수의 특징

--- day02/src/data/Test8.java
+++ day02/src/data/Test8.java
@@ -4,7 +4,7 @@ public class Test8 {
 		
 		public static void main(String[] args) {
 			//정수의 특징
-			
+		
 			//[1] 정수는 소수점이 없는 수이다.
 //			int a=3.14;
 			int a=3;
@@ -18,6 +18,17 @@ public class Test8 {
 //			int d=1000000000
 			long e=10000000000L;  //long은 표시를 해야한다.
 			System.out.println(e);
+			
+			//[4]정수는 순환형 구조를 가진다.
+			long f =1234567*1234567L; //long은 저장공간일 뿐 계산하는 게 아니기 때문에 오른쪽에 L 붙여서 계산해주기
+			System.out.println(f);
+			
+			//[5](중요)정수끼리 계산하면 정수가 나온다.
+			
+			int v1=10;
+			int v2=3;
+			System.out.println(v1/v2); //몫
+			System.out.println(v1%v2); //나머지
 			
 		
 		
