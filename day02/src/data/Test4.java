1d87eac818d57486cc1aea244a3e724e0d51aba0
wlslals0805 <wlslals0805@naver.com>
2023-06-20 13:13:37 +0900

    변수를 극대화하여 사용하는 예제

---
+++ day02/src/data/Test4.java
@@ -0,0 +1,30 @@
+package data;
+
+public class Test4 {
+		public static void main(String[] args) {
+			//변수를 제대로 사용해서 고치기 쉬운 구조로 만들기
+			
+			//[1] 문제에서 주어진 값을 먼저 저장
+		 int a=7000;
+		 int b=8000;
+		 int c=2;
+		 int d=3;
+		 
+		 //[2] 1번의 데이터로계산 
+		 
+		 int e = a*c;
+		 int f= b*d;
+		 int g= e+f;
+		 
+		 //[3] 1,2번의 데이터로 문제에서 원하는 결과를 출력
+		 
+		 System.out.println(e);
+		 System.out.println(f);
+		 System.out.println(g);
+		 
+		 
+		
+		}
+		}
+
+
---
+++ day02/src/data/Test5.java
@@ -0,0 +1,30 @@
+package data;
+
+public class Test5 {
+		public static void main (String[] args) {
+			//변수를 의미 있는 이름으로 작성하여 만들기
+			
+			int noodlePrice =7000; //변수 이름 정하는 규칙 1. 띄어쓰기 금지 언더바 혹은 대문자 사용
+			int champonPrice=8000;
+			int noodleCount=2;
+			int champonCount=3;
+			
+			 int noodleTotal = noodlePrice*noodleCount;
+			 int champonTotal= champonPrice*champonCount;
+			 int allTotal= noodleTotal+champonTotal;
+			 
+			 //[3] 1,2번의 데이터로 문제에서 원하는 결과를 출력
+			 
+			 System.out.println(noodleTotal);
+			 System.out.println(champonTotal);
+			 System.out.println(allTotal);
+			 
+			 
+		
+					
+		
+		
+		}
+		}
+
+
