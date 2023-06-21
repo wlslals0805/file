2ee200a888509e9c6616df33057758d92db7a549
wlslals0805 <wlslals0805@naver.com>
2023-06-21 10:40:19 +0900

    여행 경비 구하기 실습

---
+++ day03/src/data/Test15.java
@@ -0,0 +1,30 @@
+package data;
+
+public class Test15 {
+
+	public static void main(String[] args) {
+//		KH 여행사에서는 다음과 같이 프로모션을 진행중입니다.
+//
+//		```
+//		국내 어디든 한 사람당 1박에 10만원!  
+//		추가로 25% 할인까지 챙겨가세요!
+//		```
+//
+//		성인 3명이 7일동안 여행갈 때 예상 경비를 출력하세요
+		
+		int adult=3;
+		int day=7;
+		int price=100000;
+		
+		int totalPrice=adult*day*price;
+		int rate=25;
+		
+		int discount=totalPrice*rate/100;
+		int pay=totalPrice-discount;
+		
+		System.out.println(totalPrice);
+		System.out.println(discount);
+		System.out.println(pay);
+	}
+
+}
