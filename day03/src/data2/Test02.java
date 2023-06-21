aa2b3b7c9cf822ae7df65eabc89d4461d8f62b3a
wlslals0805 <wlslals0805@naver.com>
2023-06-21 17:18:52 +0900

    bmi 측정 실습

--- day03/src/data2/Test02.java
+++ day03/src/data2/Test02.java
@@ -4,7 +4,21 @@ public class Test02 {
 
 	public static void main(String[] args) {
 
-
+//		키가 180cm, 체중이 80kg인 사람이 있을 때 이 사람의 BMI 지수를 구하여 출력
+//
+//		               체중(kg)
+//		계산공식 = ───────────────────
+//		               키²(m²)
+	
+		int cm=180;
+		double m=cm*0.01;
+		int kg=80;
+		
+		double bmi=kg/(m*m);
+		
+		System.out.println(bmi);
+		
+		
 	}
 
 }
