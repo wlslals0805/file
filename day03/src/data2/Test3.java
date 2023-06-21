33a07ed87fd6c868c0a4298499674ba52d943a29
wlslals0805 <wlslals0805@naver.com>
2023-06-21 13:38:39 +0900

    주유소 프로그램 실습

---
+++ day03/src/data2/Test3.java
@@ -0,0 +1,23 @@
+package data2;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+
+		
+//		KH 주유소에서는 휘발유를 1L에 1530원에 팔고 있습니다.  
+//
+//		50000원 주유했을 때 주유되는 휘발유의 양을 출력하세요
+		
+		int L=1530;
+		int price=50000;
+		
+		
+		double oil=price/(double)L;
+		
+		
+		System.out.println(oil);
+		
+	}
+
+}
