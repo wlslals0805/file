6f587c482b47a3a2fba3b4f98ecad455ab60a7a9
wlslals0805 <wlslals0805@naver.com>
2023-06-22 12:23:25 +0900

    문자열(String)의 특징

--- day4/src/data4/Test02.java
+++ day4/src/data4/Test02.java
@@ -9,7 +9,7 @@ public class Test02 {
 		//주문제작 방식으로 처리된다(복잡한 데이터)	
 		//-참조변수(ex:리모컨)가 존재하며 기능(버튼)이 제공된다
 		
-		String a="hellodhtrdhfgcjty";
+		String a="hello";
 		System.out.println(a);
 		
 		//[1] 추가적으로 제공되는 기능이 존재한다
