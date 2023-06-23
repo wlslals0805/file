322f870f326fe93d536a53c300c517bd4fa993db
wlslals0805 <wlslals0805@naver.com>
2023-06-23 17:43:29 +0900

    반복 수업

---
+++ day05/src/loop/Test03.java
@@ -0,0 +1,22 @@
+package loop;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+//		1부터 100 사이의 홀수를 출력하는 프로그램을 구현하세요
+		
+		
+	
+	
+		
+		for(int i=1; i<=100;i++) {
+			
+			boolean a = i%2==1;
+			
+			if(a) {System.out.println(i);}
+			
+		}
+		
+	}
+
+}
