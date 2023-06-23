fb19e855bd10f720255a3aec71fc35d45c52609a
wlslals0805 <wlslals0805@naver.com>
2023-06-23 17:43:56 +0900

    반복 수업

---
+++ day05/src/loop/Test01.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		//반복(loop)
+		//-(Q) Hello를 화면에 10번 출력하세요
+		
+		//for(10번) {
+		for(int i=0; i <10; i++)
+		{
+		System.out.println("hello");
+		}
+	}
+
+}
