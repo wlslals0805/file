ae47f2cb888db1c2f19c5e671ca90426ccc89e24
wlslals0805 <wlslals0805@naver.com>
2023-06-23 18:09:33 +0900

    5회 입력받기 강사님 풀이

---
+++ day05/src/loop/Test02_1.java
@@ -0,0 +1,21 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Test02_1 {
+
+	public static void main(String[] args) {
+		//(주의) 도구 생성은 한 번만 한다
+				Scanner sc = new Scanner(System.in);
+				
+				for(int i=0; i < 5; i++) {
+					System.out.print("숫자 입력 : ");
+					int a = sc.nextInt();
+				
+				
+				
+
+	}
+				sc.close();
+	}
+}
