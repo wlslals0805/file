d80384ed22fb83786697d0a5460b3ae063591fa2
wlslals0805 <wlslals0805@naver.com>
2023-06-23 17:43:45 +0900

    반복 실습

---
+++ day05/src/loop/Test02.java
@@ -0,0 +1,21 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+//		사용자에게 숫자를 5번 입력받는 프로그램을 구현하세요
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int a=0; a<5; a++) {
+		 System.out.println("숫자를 다섯 번 입력하세요");
+			int f=sc.nextInt();
+			System.out.println(f);
+		}
+		
+		
+	}
+
+}
