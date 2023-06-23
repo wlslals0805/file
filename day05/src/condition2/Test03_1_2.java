f2ed520a9dcaa74ed8278832aeb4a6eb9ddbcb9e
wlslals0805 <wlslals0805@naver.com>
2023-06-23 18:10:31 +0900

    지하철 이용요금 실습 카드 보증금,다섯정거장마다 요금 추가

---
+++ day05/src/condition2/Test03_1_2.java
@@ -0,0 +1,55 @@
+package condition2;
+
+import java.util.Scanner;
+
+public class Test03_1_2 {
+
+	public static void main(String[] args) {
+		
+//		지하철 이용요금은 다음과 같습니다
+//
+//		| 구분 | 기준 | 요금 |
+//		| --- | --- | --- |
+//		| 어르신 | 65세 이상 | 무료 |
+//		| 성인 | 20세 이상 65세 미만 | 1250원 |
+//		| 청소년 | 14세 이상 20세 미만 | 720원 |
+//		| 어린이 | 8세 이상 14세 미만 | 450원 |
+//		| 영유아 | 8세 미만 | 0원 |
+//
+//		사용자에게 나이를 입력받아 지하철 요금을 출력하세요
+		//카드 보증금 500원 추가
+		//다섯정거장마다 100원씩 추가 요금
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("나이를 입력하세요");
+		int age = sc.nextInt();
+		
+		System.out.println("몇번 째 정거장에 가는지 입력하세요");
+		int bus=sc.nextInt();
+		int card=500;
+		int price; 
+		int addPrice=100;
+		int addCount=bus/6;  
+		
+		
+		
+		if(65<=age||age<8) {
+			price=0;
+		}
+		else if(age>=20){
+			price=1250;
+			
+		}
+		else if(age>=14) {
+			price=720;
+		}
+		else {
+			price=450;
+		}
+		
+		int total=price+card+(addCount*addPrice);
+		System.out.println(total);
+	}
+
+}
