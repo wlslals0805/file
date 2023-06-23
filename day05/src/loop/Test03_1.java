354d92c9b41a0f917562d02602d91179f87903ca
wlslals0805 <wlslals0805@naver.com>
2023-06-23 18:07:24 +0900

    1부터 100사이 홀수 출력 강사님 풀이

---
+++ day05/src/loop/Test03_1.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class Test03_1 {
+
+	public static void main(String[] args) {
+
+		//1부터 100사이 홀수 출력
+
+				for(int i=1; i <=100; i++) {
+					if(i % 2 != 0) {//i가 홀수면
+						System.out.println(i);
+	}
+				}
+	}
+}
