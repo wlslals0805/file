e29abea112ee7de261f722d96b95ba36a93d0d4b
wlslals0805 <wlslals0805@naver.com>
2023-06-23 10:05:51 +0900

    과속 단속 프로그램 강사님 풀이

--- day4/src/condition/Test05_1.java
+++ day4/src/condition/Test05_1.java
@@ -1,11 +1,38 @@
 package condition;
 
+import java.util.Scanner;
+
 public class Test05_1 {
 
 	public static void main(String[] args) {
 		//과속 단속 카메라
 		
-		
+		//입력
+				Scanner sc = new Scanner(System.in);
+
+				System.out.print("속도(km) : ");
+				int speed = sc.nextInt();
+
+				sc.close();
+
+				int limit = 50;
+
+				//계산
+				int overSpeed = speed - limit;
+				boolean over = speed > limit;
+
+				//int fine = 0 또는 어쩌구저쩌구;
+				int fine;
+				if(over) {
+					fine = 30000 + overSpeed / 10 * 10000;
+				}
+				else {
+					fine = 0;
+				}
+
+
+				//출력
+				System.out.println("벌금 : " + fine+"원");
 	}
 
 }
