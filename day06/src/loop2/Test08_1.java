6e65c34240a02e84d2d7810eac35c7de3c098c42
wlslals0805 <wlslals0805@naver.com>
2023-06-26 16:59:51 +0900

    현명한 노인 문제 강사님 풀이

---
+++ day06/src/loop2/Test08_1.java
@@ -0,0 +1,9 @@
+package loop2;
+
+public class Test08_1 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ day06/src/loop2/Test08_2.java
@@ -0,0 +1,10 @@
+package loop2;
+
+public class Test08_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
