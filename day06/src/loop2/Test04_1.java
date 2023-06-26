171898ef152ddd3f17238dc4c2c051fe0f4d1bb5
wlslals0805 <wlslals0805@naver.com>
2023-06-26 18:16:17 +0900

    소수 판정 프로그램 단계별 풀이

---
+++ day06/src/loop2/Test04_1.java
@@ -0,0 +1,18 @@
+package loop2;
+
+public class Test04_1 {
+
+	public static void main(String[] args) {
+		//입력
+				int number = 7;
+
+				System.out.println(number % 1);
+				System.out.println(number % 2);
+				System.out.println(number % 3);
+				System.out.println(number % 4);
+				System.out.println(number % 5);
+				System.out.println(number % 6);
+				System.out.println(number % 7);
+	}
+
+}
