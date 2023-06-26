23c7fb793c1877dca1b1aaf82c2bfdee2d40c220
wlslals0805 <wlslals0805@naver.com>
2023-06-26 18:17:36 +0900

    누적 합계 구하기 수업

---
+++ day06/src/loop2/Test06.java
@@ -0,0 +1,30 @@
+package loop2;
+
+public class Test06 {
+
+	public static void main(String[] args) {
+		//누적 합계 구하기
+		//(Q1) 10을 10번 더하면 얼마예요?
+		//(Q2)1부터 10까지 더하면 얼마에요?
+		
+		int total = 0;
+//		total+=10;
+//		total+=10;
+//		total+=10;
+//		total+=10;
+//		total+=10;
+//		total+=10;
+//		total+=10;
+//		total+=10;
+//		total+=10;
+//		total+=10;
+//		System.out.println(total);
+		for(int i=1;i<=10;i++) {
+		total +=10;
+		}
+		System.out.println(total);
+		
+		
+	}
+
+}
