c58b50dd782ff8552a7f9fceb96e7d0660add8fb
wlslals0805 <wlslals0805@naver.com>
2023-06-27 14:31:23 +0900

    현명한 노인문제 강사님 풀이

--- day06/src/loop2/Test08_1.java
+++ day06/src/loop2/Test08_1.java
@@ -3,6 +3,19 @@ package loop2;
 public class Test08_1 {
 
 	public static void main(String[] args) {
+		//현명한 노인 문제
+		
+				int money = 1;//1원으로 시작
+				int total = 0;//합계는 최초 0
+				
+				for(int day=1; day<=30; day++) {
+					System.out.println(day+"일차 = "+money+"원");
+					total += money;//돈을 누적시켜라
+					
+					money *= 2;//돈을 두 배로 증가시켜라
+				}
+				
+				System.out.println("총 받는 금액 = " + total + "원");
 		
 	}
 
--- day06/src/loop2/Test08_2.java
+++ day06/src/loop2/Test08_2.java
@@ -3,8 +3,19 @@ package loop2;
 public class Test08_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//현명한 노인 문제
+		
+				long money = 1L;//1원으로 시작
+				long total = 0L;//합계는 최초 0
+				
+				for(int day=1; day<=40; day++) {
+					System.out.println(day+"일차 = "+money+"원");
+					total += money;//돈을 누적시켜라
+					
+					money *= 2;//돈을 두 배로 증가시켜라
+				}
+				
+				System.out.println("총 받는 금액 = " + total + "원");
 	}
 
 }
