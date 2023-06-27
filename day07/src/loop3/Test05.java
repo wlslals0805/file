2dcc91e102d29d2474f87eba925630ed455fc562
wlslals0805 <wlslals0805@naver.com>
2023-06-27 17:22:46 +0900

    숫자 모래성 땅따먹기 게임 실습

--- day07/src/loop3/Test05.java
+++ day07/src/loop3/Test05.java
@@ -14,15 +14,21 @@ public class Test05 {
 //		차감을 하다가 0 이하가 되면 반복을 종료하고 게임 오버 메세지를 출력
 //
 //		몇 번만에 게임오버가 되었는지 측정하여 출력
+//		원하는 상황일 때만 코드를 실행하도록 구현
 		
 		Scanner sc=new Scanner(System.in);
 		int a=100;
 		int count=0;
 		
 		while(true) {
+			System.out.println("1~9까지의 숫자 아무거나 입력:");
 		int number=sc.nextInt();
+		
+		if(number<=9&&number>=1) {
 		a=a-number;
 		count++;
+		
+		System.out.println("남은 숫자="+a);
 		
 		
 		if(a<=0) {
@@ -30,6 +36,9 @@ public class Test05 {
 			break;
 		}
 	}
+		else {System.out.println("1~9 사이의 숫자만 입력하세요");}
+		}
+	
 		sc.close();
 		System.out.println("입력한 횟수는 "+count+"번");
 	}
