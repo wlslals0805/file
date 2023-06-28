b8661204c05748fab314e420db9c1d9adb2f8ede
wlslals0805 <wlslals0805@naver.com>
2023-06-28 10:49:34 +0900

    실수 배열 생성 실습

---
+++ day08/src/array/Test03.java
@@ -0,0 +1,26 @@
+package array;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+//		'1.5', '2.4', '3.6'을 저장하기 위한 배열을 생성하고 데이터를 저장한 뒤 출력하세요
+		
+		
+		//배열 생성
+		//data-------->[0.0][0.0][0.0]
+		
+		double[] a=new double[3];
+		
+		a[0]=1.5;
+		a[1]=2.4;
+		a[2]=3.6;
+		
+		
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		
+		
+	}
+
+}
