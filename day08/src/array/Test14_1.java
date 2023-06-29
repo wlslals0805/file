b3635efd6605a5b189faf355d2fa7bad19cc8456
wlslals0805 <wlslals0805@naver.com>
2023-06-29 10:01:20 +0900

    배열 셔플 문제 강사님 풀이

---
+++ day08/src/array/Test14_1.java
@@ -0,0 +1,26 @@
+package array;
+
+public class Test14_1 {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+
+		//데이터 준비
+		int[] data = new int[] {30, 50, 20, 10, 40};
+
+		//셔플(뒤섞기)
+		//[0] <---> [0~4]
+		int index = r.nextInt(data.length);
+		System.out.println("index = " + index);
+
+		int backup = data[0];
+		data[0] = data[index];
+		data[index] = backup;
+
+		//출력
+		for(int i=0; i < data.length; i++) {
+			System.out.println(data[i]);
+		}
+	}
+
+}
