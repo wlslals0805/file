e23e351355303614c55ec136457fadaf82149162
wlslals0805 <wlslals0805@naver.com>
2023-07-03 09:49:07 +0900

    day11 생성

--- day11/src/oop/setter1/Student.java
+++ day11/src/oop/setter1/Student.java
@@ -1,5 +1,24 @@
 package oop.setter1;
 
 public class Student {
+	
+	//필드가 만들어져야 클래스의 나머지 형태를 구성할 수 있다
+	
+	String name;
+	int score;
+	
+	//메소드 - setup, show
+	void setup(String name, int score) {
+		
+		this.name = name;
+		this.score = score;
+		
+	}
+	void show() {
+		System.out.println(this.name);
+		System.out.println(this.score);
 
+	}
+	
+	
 }
--- day11/src/oop/setter1/Test01.java
+++ day11/src/oop/setter1/Test01.java
@@ -3,8 +3,13 @@ package oop.setter1;
 public class Test01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Student a = new Student();
+		
+		a.setup("피카츄", 70);
+		
+		a.show();
+		
+		
 	}
 
 }
