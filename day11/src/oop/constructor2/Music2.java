c535d54bd0414210f4810837f513013f8ce794b3
wlslals0805 <wlslals0805@naver.com>
2023-07-03 17:16:23 +0900

    음원 차트 문제 강사님 풀이

--- day11/src/oop/constructor2/Music2.java
+++ day11/src/oop/constructor2/Music2.java
@@ -1,5 +1,91 @@
 package oop.constructor2;
 
 public class Music2 {
+	//멤버 필드 - 데이터
+		String title, artist, album;
+		long play;
+		int like;
+		
+		//멤버 메소드 - 코드
+		void setTitle(String title) {
+			this.title = title;
+		}
+		void setArtist(String artist) {
+			this.artist = artist;
+		}
+		void setAlbum(String album) {
+			this.album = album;
+		}
+		void setPlay(long play) {
+			if(play < 0L) return;
+			this.play = play;
+		}
+		void setLike(int like) {
+			if(like < 0) return;
+			this.like = like;
+		}
+		
+		String getTitle() {
+			return this.title;
+		}
+		String getArtist() {
+			if(this.artist == null) {
+				return "Various artist";
+			}
+			else {
+				return this.artist;
+			}
+		}
+		String getAlbum() {
+			return this.album;
+		}
+		long getPlay() {
+			return this.play;
+		}
+		int getLike() {
+			return this.like;
+		}
+		
+		String getBest() {
+			if(this.play > 100000L) {
+				return "(베스트)";
+			}
+			else {
+				return "";
+			}
+		}
+		String getHot() {
+			if(this.like > 100000) {
+				return "(인기곡)";
+			}
+			else {
+				return "";
+			}
+		}
+		long getRankPoint() {
+			return this.play * 2 + this.like / 2;
+		}
+		
+		//생성자 - 초기화
+//		Music(String title, String artist, String album) {
+//			this(title, artist, album, 0L, 0);//아래 생성자로 데이터 전달
+//		}
+//		Music(String title, String artist, String album, long play, int like) {
+//			this.setTitle(title);
+//			this.setArtist(artist);
+//			this.setAlbum(album);
+//			this.setPlay(play);
+//			this.setLike(like);
+//		}
+		
+		void show() {
+			System.out.println("<음원 정보>");
+			System.out.println("제목 : " + this.getTitle() + this.getBest() + this.getHot());
+			System.out.println("가수 : " + this.getArtist());
+			System.out.println("앨범 : " + this.getAlbum());
+			System.out.println("재생수 : " + this.getPlay());
+			System.out.println("좋아요 : " + this.getLike());
+			System.out.println("랭킹포인트 : " + this.getRankPoint());
+		}
 
 }
--- day11/src/oop/constructor2/Test01_1.java
+++ day11/src/oop/constructor2/Test01_1.java
@@ -3,8 +3,17 @@ package oop.constructor2;
 public class Test01_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//		Music a = new Music("그때 그 순간 그대로(그그그)", "WSG워너비", "WSG워너비 1집", 104250L, 111835);
+		Music b = new Music("보고싶었어", "WSG워너비", "WSG워너비 1집", 83127, 90805);
+		Music c = new Music("LOVE DIVE", "IVE(아이브)", "LOVE DIVE", 64590, 174519);
+		Music d = new Music("POP!", "나연(TWICE)", "IM NAYEON", 58826, 70313);
+		Music e = new Music("애국가", null, "애국가", 10000, 20000);
+		
+//		a.show();
+		b.show();
+		c.show();
+		d.show();
+		e.show();
 	}
 
 }
