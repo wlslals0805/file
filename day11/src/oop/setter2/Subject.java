142cee86174cb50189e513eba9917872708642d3
wlslals0805 <wlslals0805@naver.com>
2023-07-03 13:13:13 +0900

    학원 강의 정보 강사님 풀이

--- day11/src/oop/setter2/Subject.java
+++ day11/src/oop/setter2/Subject.java
@@ -1,5 +1,51 @@
 package oop.setter2;
 
 public class Subject {
-
+	String title;//강좌명
+	int period;//강의시간
+	int price;//수강료
+	String type;//구분
+	
+	void setTitle(String title) {
+		this.title = title;
+	}
+	void setPeriod(int period) {
+		if(period > 0 && period % 30 == 0) {
+			this.period = period;
+		}
+	}
+//	void setPrice(int price) {
+//		if(price >= 0) {
+//			this.price = price;
+//		}
+//	}
+	void setPrice(int price) {
+		if(price < 0) {
+			return;//반환은 없지만 중지하세요!
+		}
+		this.price = price;
+	}
+	void setType(String type) {
+		switch(type) {
+		case "온라인":
+		case "오프라인":
+		case "혼합":
+			this.type = type;
+		}
+	}
+	
+	void setup(String title, int period, int price, String type) {
+		this.setTitle(title);
+		this.setPeriod(period);
+		this.setPrice(price);
+		this.setType(type);
+	}
+	
+	void show() {
+		System.out.println("<강좌 정보>");
+		System.out.println("이름 : " + this.title);
+		System.out.println("시간 : " + this.period);
+		System.out.println("가격 : " + this.price+"원");
+		System.out.println("유형 : " + this.type);
+	}
 }
--- day11/src/oop/setter2/Test01_1.java
+++ day11/src/oop/setter2/Test01_1.java
@@ -3,8 +3,17 @@ package oop.setter2;
 public class Test01_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Subject s1 = new Subject();
+		Subject s2 = new Subject();
+		Subject s3 = new Subject();
+		
+		s1.setup("자바 마스터과정", 90, 1000000, "온라인");
+		s2.setup("파이썬 기초", 60, 600000, "온라인");
+		s3.setup("웹 개발자 단기완성", 120, 120000, "오프라인");
+		
+		s1.show();
+		s2.show();
+		s3.show();
 	}
 
 }
