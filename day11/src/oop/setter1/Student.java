c7fd6a15135a8b1e3639ac9c804fd3b7ad488462
wlslals0805 <wlslals0805@naver.com>
2023-07-03 13:13:39 +0900

    setter 메소드의 사용

--- day11/src/oop/setter1/Student.java
+++ day11/src/oop/setter1/Student.java
@@ -10,8 +10,8 @@ public class Student {
 	//메소드 - setup, show
 	void setup(String name, int score) {
 		
-		this.name = name;
-		this.score = score;
+		this.setName(name);//내가 가진 setName 메소드에 name을 전달하며 호출
+		this.setScore(score);//내가 가진 setScore메소드에 score을 전달하며 호출
 		
 	}
 	void show() {
@@ -19,6 +19,24 @@ public class Student {
 		System.out.println(this.score);
 
 	}
+	//필드를 하나씩 변경할 수 있는 메소드를 구현
+	//-setter 메소드
+	//-set + 필드명으로 이름 짓는다.
+	//-설정 외에 다른 작업을 하지 않는다.
+	//-(중요) 올바른 데이터만 설정 가능하도록 필터링 처리를 한다
+	void setName(String name) {
+		switch(name) { //문자열은 비교를 switch로 한다(if로 하는 방법은 나중에)
+		case "피카츄":
+		case "라이츄":
+			this.name = name;	
+		}		
+	}
+	
+	void setScore(int score) {
+		if(score>=0&&score<=100) {
+		this.score = score;
+	}
 	
 	
+	}
 }
--- day11/src/oop/setter1/Test02.java
+++ day11/src/oop/setter1/Test02.java
@@ -1,6 +1,6 @@
 package oop.setter1;
 
-public class Test01 {
+public class Test02 {
 
 	public static void main(String[] args) {
 		Student a = new Student();
@@ -13,11 +13,13 @@ public class Test01 {
 		
 		//[2] 하나의 정보만 바꿀 수 없다.
 		//->데이터를 하나만 설정할 수 있는 메소드를 만든다(setter 메소드)
-		a.setup("피카츄",50);	//점수를 바꾸고 싶은데 이름까지 적어야 한다
+//		a.setup("피카츄",50);	//점수를 바꾸고 싶은데 이름까지 적어야 한다
+		a.setScore(50);
 		a.show();
 		
 		
-		a.setup("라이츄",50); //이름을 바꾸고 싶은데 점수까지 적어야 한다
+//		a.setup("라이츄",50); //이름을 바꾸고 싶은데 점수까지 적어야 한다
+		a.setName("라이츄");
 		a.show();
 		
 	
