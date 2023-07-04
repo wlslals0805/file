8e2536fbbc0ddff1e58ab6ae512c9e59d5b43f7f
wlslals0805 <wlslals0805@naver.com>
2023-07-04 12:33:33 +0900

    static 키워드를 이용한 간편 메소드 구현

---
+++ day12/src/oop/keyword2/Calculator.java
@@ -0,0 +1,13 @@
+package oop.keyword2;
+
+public class Calculator {
+	
+	//문제점
+	//[1] 내부 구조가 복잡함 - 필드를 제거
+	//[2] 객체를 꼭 만들어야함
+
+		public int plus(int left, int right) {
+			return left + right;
+		
+		}
+}
---
+++ day12/src/oop/keyword2/Test01.java
@@ -0,0 +1,11 @@
+package oop.keyword2;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		Calculator a = new Calculator();
+		int result = a.plus(10, 20);
+		System.out.println("result = " + result);
+	}
+
+}
