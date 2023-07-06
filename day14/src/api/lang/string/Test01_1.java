7e88ddb7cd11ec816cf13f264654f095464b5e27
wlslals0805 <wlslals0805@naver.com>
2023-07-06 17:20:05 +0900

    문자열 생성 강사님 풀이

---
+++ day14/src/api/lang/string/Test01_1.java
@@ -0,0 +1,30 @@
+package api.lang.string;
+
+public class Test01_1 {
+
+	public static void main(String[] args) {
+		//문자열(String)
+		
+				//생성
+				String a = "hello";//기존 방법(클래스의 문법 아님, 특혜)
+				
+				char[] ch = new char[] {'h','e','l','l','o'};
+				String b = new String(ch);
+				
+				String c = new String("hello");
+				
+				System.out.println(a.toString());
+				System.out.println(a);
+				System.out.println(b);
+				System.out.println(c);
+				
+				//기능 사용
+				System.out.println(a.length());
+				System.out.println(b.length());
+				
+				//a와 b는 같나?
+				System.out.println(a == b);//동일한 객체인지 비교(비추천)
+				System.out.println(a.equals(b));//동등 비교(내용이 같은지 비교, 추천)
+	}
+
+}
