c5652cba294afc3460b6116a2c51d3a7abc2735d
wlslals0805 <wlslals0805@naver.com>
2023-07-06 14:26:17 +0900

    다형성 코드 추가

--- day13/src/oop/inherit8_2/Interaction.java
+++ day13/src/oop/inherit8_2/Interaction.java
@@ -5,7 +5,8 @@ package oop.inherit8_2;
 public class Interaction {
 	
 	//전투 기능
-	public static void battle(Player a,Player b) {
+	public static void battle(Player a,Player b) //Player 클래스를 상속받는 객체들이 들어올 수 있음 
+	{
 		
 		a.attack();
 		b.attack();
