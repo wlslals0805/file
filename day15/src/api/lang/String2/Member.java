385fae4135a7f6611d260df99997834f41835ff1
wlslals0805 <wlslals0805@naver.com>
2023-07-10 10:04:32 +0900

    day15

--- day15/src/api/lang/String2/Member.java
+++ day15/src/api/lang/String2/Member.java
@@ -2,45 +2,77 @@ package api.lang.String2;
 
 public class Member {
 
+//	회원 가입 프로그램
+//
+//	Member라는 클래스를 만들고 다음 정보를 보관할 수 있도록 구현합니다.
+//
+//	회원아이디(memberId) - 8~20자 알파벳+숫자 로 구성
+//	회원비밀번호(memberPw) - 8~15자 알파벳+숫자+특수문자 반드시 포함하여 구성
+//	회원이름(memberName) - 한국인 이름
+//	회원등급(memberLevel) - 관리자 / 우수회원 / 일반회원 중 하나로 설정, 기본값은 일반회원
+//	회원포인트(memberPoint) - 최초 100점을 부여, 0 이상만 가능
+//	Scanner를 통해서 가입에 필요한 정보를 입력받아 객체를 생성하고 출력
+//	단, 비밀번호는 첫 글자를 제외한 나머지 마스킹 처리하여 출력
+	
+
 	String memberId;
 	String memberPw;
 	String memberName;
 	String memberLevel;
-	String memberPoint;
+	int memberPoint;
 //	
 	public String getMemberId() {
 		return memberId;
 	}
 	public void setMemberId(String memberId) {
-		this.memberId = "^[A-Z0-9]{8,20}";
+		if(memberId.matches("^[A-Za-z0-9]{8,20}$")){
+		this.memberId = "아이디:" + memberId;}
+		else {this.memberId="아이디: 승인불가";}
+		
 	}
+		
 	public String getMemberPw() {
-		return memberPw;
+		
+		int count = memberPw.length();
+		String star = "*";
+		return	memberPw.charAt(0)+memberPw.replace(memberPw,star.repeat(count-1));
+		
 	}
 	public void setMemberPw(String memberPw) {
-		this.memberPw = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$])[A-Z0-9!@#$]{8,15}";
-	}
+if(memberPw.matches("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$])[A-Za-z0-9!@#$]{8,15}$")) {
+	this.memberPw = memberPw;}
+}
+	
 	public String getMemberName() {
 		return memberName;
 	}
 	public void setMemberName(String memberName) {
-		this.memberName = "^[가-힣]{2,7}$";
+		if(memberName.matches("^[가-힣]{2,7}$")) {
+		this.memberName = memberName;}
+		else {this.memberName="이름 승인불가";}
 	}
 	public String getMemberLevel() {
 		return memberLevel;
 	}
 	public void setMemberLevel(String memberLevel) {
-		this.memberLevel = "^(관리자|우수회원|일반회원)$";
+		if(memberLevel.matches("^(관리자|우수회원|일반회원)$")) {
+		this.memberLevel = memberLevel;}
+		else {this.memberLevel="일반회원";}
 	}
-	public String getMemberPoint() {
-		return memberPoint;
+	public int getMemberPoint() {
+		
+		return this.memberPoint+100;
 	}
-	public void setMemberPoint(String memberPoint) {
-		this.memberPoint = "^[1][0][0][1-9]$";
+	public void setMemberPoint(int memberPoint) {
+		
+		if(memberPoint>0) {
+		this.memberPoint=memberPoint;}
+		
+		
 	}
 	
 	
-	Member(String memberId,String memberPw,String memberName,String memberLevel,String memberPoint){
+	Member(String memberId,String memberPw,String memberName,String memberLevel,int memberPoint){
 		
 		this.setMemberId(memberId);
 		this.setMemberPw(memberPw);
@@ -48,7 +80,30 @@ public class Member {
 		this.setMemberLevel(memberLevel);
 		this.setMemberPoint(memberPoint);
 		
+	
+}
 		
-	}
+		
+		void show(){
+			
+			
+			System.out.println(memberId);
+		
+			System.out.println("비밀번호:" + this.getMemberPw());
+			
+			System.out.println("이름:" + this.memberName);
+			
+			System.out.println("회원등급:"+this.memberLevel);
+			
+			System.out.println("회원 포인트:"+getMemberPoint());
+			
+			
+			
+			
+				
+		
+	
+	
+}
 	
 }
