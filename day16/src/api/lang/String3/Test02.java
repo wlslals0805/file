78cf5d6dde4edb927d0c394b362fb770652fbe7e
wlslals0805 <wlslals0805@naver.com>
2023-07-10 12:26:02 +0900

    문자열 합성 도구

--- day16/src/api/lang/String3/Test02.java
+++ day16/src/api/lang/String3/Test02.java
@@ -5,18 +5,17 @@ public class Test02 {
 	public static void main(String[] args) {
 		//문자열 덧셈 성능 측정
 		
-		long start = System.currentTimeMillis();	//측정 시작
-		
-		String star = "";
-		
-		for(int i=0;i<10000;i++) {
-		star+="*";}
-		
-		
-		long finish = System.currentTimeMillis();	//측정 완료
-		
-		long time = finish - start;
-		System.out.println("time="+time);
+				long start = System.currentTimeMillis();//측정 시작
+				
+				String star = "";
+				for(int i=0; i < 1000000; i++) {
+					star += "*";
+				}
+				
+				long finish = System.currentTimeMillis();//측정 완료
+				
+				long time = finish - start;
+				System.out.println("time = " + time);
 	}
 
 }
--- day16/src/api/lang/String3/Test02_2.java
+++ day16/src/api/lang/String3/Test02_2.java
@@ -4,19 +4,22 @@ public class Test02_2 {
 
 	public static void main(String[] args) {
 		//문자열 덧셈 성능 측정
-		
-		long start = System.currentTimeMillis();	//측정 시작
-		
-		StringBuilder buffer = new StringBuilder();	
-		for(int i=0;i<10000000;i++) {
-		buffer.append("*");}
-		
-		String star = buffer.toString();
-		
-		long finish = System.currentTimeMillis();	//측정 완료
-		
-		long time = finish - start;
-		System.out.println("time="+time);
+				//- 싱글 스레드 환경에서는 StringBuilder가 효율적(잠금장치가 없으니까)
+				//- 멀티 스레드 환경에서는 StringBuffer가 효율적(느리지만 안전)
+				
+				long start = System.currentTimeMillis();//측정 시작
+				
+				StringBuilder buffer = new StringBuilder();
+				for(int i=0; i < 10; i++) {
+					buffer.append("*");
+				}
+				String star = buffer.toString();
+				
+				long finish = System.currentTimeMillis();//측정 완료
+				
+				long time = finish - start;
+				System.out.println("time = " + time);
+				
 	}
 
 }
