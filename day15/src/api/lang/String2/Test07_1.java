1e8d545c2e214e31148fe35fe204ffb6d60c728d
wlslals0805 <wlslals0805@naver.com>
2023-07-10 10:22:22 +0900

    day 15 과제 강사님 풀이

--- day15/src/api/lang/String2/Test07_1.java
+++ day15/src/api/lang/String2/Test07_1.java
@@ -7,6 +7,7 @@ public class Test07_1 {
 		String memberId = "testuser1234";
 		String memberPw = "Testuser1234!";
 		String memberName = "테스트";
+	
 		
 		Member member = new Member(memberId, memberPw, memberName, memberName, 0);
 		member.show();
