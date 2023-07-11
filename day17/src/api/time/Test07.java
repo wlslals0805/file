57e2621374e4b42a3dc03edf1a99860e4d0a1973
wlslals0805 <wlslals0805@naver.com>
2023-07-11 21:02:56 +0900

    달력 만들기 실습

--- day17/src/api/time/Test07.java
+++ day17/src/api/time/Test07.java
@@ -1,33 +1,48 @@
 package api.time;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Scanner;
+import java.util.Date;
 
 public class Test07 {
 
 	public static void main(String[] args) {
 //		사용자에게 연도와 월을 입력받아서 해당하는 달 달력의 시작날짜를 출력
 //
-//		2023년 7월 달력의 시작날짜는 6/25(일) 입니다
-		Calendar c = Calendar.getInstance();
-		Scanner sc=new Scanner(System.in);
+//		2023년 7월 달력의 시작날짜(달력에 처음으로 뜨는 날짜)는 6/25(일) 입니다
 		
-		String a=sc.next();
+		Calendar c=Calendar.getInstance();
 		
-		String year =  a.substring(0, 4);
-		String month =  a.substring(4,6);
+		int inputY =2023;
+		int inputM=07;
 		
-		int v=Integer.parseInt(year);
-		int b=Integer.parseInt(month);
-		
-		
-		c.set(v, b,1);
+		c.set(inputY, inputM-1, 1);
 		
 		System.out.println(c.get(Calendar.YEAR));
 		System.out.println(c.get(Calendar.MONTH));
 		System.out.println(c.get(Calendar.DATE));
+		
+		
 		System.out.println(c.get(Calendar.DAY_OF_WEEK));
-
+		
+		int week=c.get(Calendar.DAY_OF_WEEK)-1;
+		
+		
+		
+		c.set(Calendar.DATE, c.get(Calendar.DATE)-week);
+		
+		
+		
+		
+		Date d=c.getTime();
+		
+	SimpleDateFormat fmt=new SimpleDateFormat("yyyy-MM-dd E");
+		
+		System.out.println(fmt.format(d));
+		
+		
+		
+		
 		
 		
 		
