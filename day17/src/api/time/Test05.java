172aad3f78ea14ffd128850d2769e1bd19c47fcb
wlslals0805 <wlslals0805@naver.com>
2023-07-11 21:02:16 +0900

    Calendar 문제

--- day17/src/api/time/Test05.java
+++ day17/src/api/time/Test05.java
@@ -1,6 +1,8 @@
 package api.time;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
 
 public class Test05 {
@@ -8,14 +10,23 @@ public class Test05 {
 	public static void main(String[] args) {
 //		올해 중에서 랜덤으로 하루를 추첨하여 출력
 		
-		Calendar c = Calendar.getInstance();
+		Calendar c=Calendar.getInstance();
 		Random r= new Random();
 		
-		System.out.println("랜덤:"+c.get(r.nextInt(Calendar.DAY_OF_YEAR))+1);
+		int day=r.nextInt(365)+1;		
+		
+		c.set(2023, 0, day);		//365일 중에서 하루를 빼내면 Calendar가 자동으로 계산해서 날짜로 변환시켜준다.
+		
+		Date d=c.getTime();
+		
+		SimpleDateFormat fmt=new SimpleDateFormat("yyyy-MM-dd");
+		
+		System.out.println(fmt.format(d));
+		
+			
 		
 		
 		
-
 	}
 
 }
--- day17/src/api/time/Test06.java
+++ day17/src/api/time/Test06.java
@@ -9,18 +9,22 @@ public class Test06 {
 	public static void main(String[] args) {
 //		오늘부터 100일동안 다이어트를 한다고 했을 때 다이어트가 끝나는 날을 구하여 출력
 		
-		Calendar c = Calendar.getInstance();
+		Calendar c=Calendar.getInstance();
+			
 		
-		int today=c.get(Calendar.DATE);
-
-		System.out.println(today);
-
-		c.set(Calendar.DAY_OF_MONTH, today+99);
-				
-		System.out.println(c.get(Calendar.DATE));
+		//Calendar.DATE와 c.get(Calendar.DATE)의 값은 다르다. c라는 참조변수로 인해서 
+		//c.get(Calendar.DATE)는 객체 생성 당시의 값을 반영. Calendar.DATE는 무슨 값을 반영하는지는 모르겠음.
+		
+		int t=c.get(Calendar.DATE);
+		
+		c.set(Calendar.DATE, t+99);
+		
 		
 		Date d= c.getTime();
-		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd E");
+		
+		
+		SimpleDateFormat fmt=new SimpleDateFormat("yyyy-MM-dd E");
+		
 		System.out.println(fmt.format(d));
 		
 	}
