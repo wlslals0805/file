958499db5caa3720e1b598440d69df43e9f4af99
wlslals0805 <wlslals0805@naver.com>
2023-07-11 21:01:47 +0900

    Calendar 예제

--- day17/src/api/time/Test03.java
+++ day17/src/api/time/Test03.java
@@ -9,16 +9,17 @@ public class Test03 {
 	public static void main(String[] args) {
 		//Calendar 클래스
 		//Date에 비해 가진 정보가 엄청 많다
+		//Date로 변환이 가능
 		
-//		Calendar c= new Calendar(); //=추상 클래스이므로 객체 생성 불가
+//		Calendar c= new Calendar(); //=추상 클래스이므로 객체 생성 불가(객체 형식으로 불러오는 게 아님)
 		
-		
-//		Calendar c = new GregorianCalendar();		//업캐스팅
+//		Calendar c = new GregorianCalendar();		//(객체 생성 가능)업캐스팅
 		Calendar c = Calendar.getInstance(); //권장하는 방법(생성위탁)
+			
+		System.out.println(c);	//Calendar 클래스가 가지고 있는 정보들 나열
 		
-		System.out.println(c);
 		
-		Date d = c.getTime();
+		Date d = c.getTime(); 	//Date로 변환
 		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd E H:mm:ss");
 		System.out.println(fmt.format(d));
 		
