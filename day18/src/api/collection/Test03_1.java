f49df0499e900fe80db044468fce5deaf76a9300
wlslals0805 <wlslals0805@naver.com>
2023-07-12 21:02:24 +0900

    끝말잇기 게임 강사님 풀이

--- day18/src/api/collection/Test03_1.java
+++ day18/src/api/collection/Test03_1.java
@@ -1,10 +1,37 @@
 package api.collection;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Test03_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//저장소 생성
+		ArrayList<String> history = new ArrayList<>();
+		
+		//입력도구 생성
+		Scanner sc = new Scanner(System.in);
+		
+		//사용자 입력
+		while(true) {
+			System.out.print("나라 이름 : ");
+			String name = sc.next();
+			
+			//if(저장소에 이미 추가된 이름이라면) {
+			if(history.contains(name)) {
+				break;
+			}
+			else {
+				history.add(name);
+			}
+			
+		}
+		
+		sc.close();
+		
+		System.out.println("게임 오버!");
+		System.out.println("총 "+history.size()+"개의 나라를 입력했습니다");
 	}
 
 }
