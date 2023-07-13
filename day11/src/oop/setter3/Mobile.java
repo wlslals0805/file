9d431b4283a553dd7a570bcb63b2756b0e18945c
wlslals0805 <wlslals0805@naver.com>
2023-07-13 20:53:14 +0900

    휴대폰 판매정보 강사님 풀이(메모 추가)

--- day11/src/oop/setter3/Mobile.java
+++ day11/src/oop/setter3/Mobile.java
@@ -42,8 +42,8 @@ public class Mobile {
 	
 	//메소드 오버로딩(method overloading)
 	void setup(String name, String telecom, int price) {
-		this.setup(name, telecom, price, 0);
-	}
+		this.setup(name, telecom, price, 0);	//메인 메소드에서 객체 생성 후 참조변수.setup을 불러내고 매개변수에 값을 입력하면 알맞는 곳에 값이 저장되면서 각각의 메소드가 실행되는 것처럼 여기서는 this를 참조변수처럼 사용하며 setup메소드를 불러내고 값을 입력하는 것(마치 메인 메소드인 것처럼 구는 것) 
+	}															//그니까 한마디로 괄호 안에 있는 0이 this.setMonth(0)이 되는 것.
 	void setup(String name, String telecom, int price, int month) {
 		this.setName(name);
 		this.setTelecom(telecom);
@@ -62,7 +62,7 @@ public class Mobile {
 			System.out.println("(원가 " + this.price +"원)");
 		}
 		else {//약정이 있다면
-			int monthPrice = this.price / this.month;
+			int monthPrice = this.price / this.month;			//임시로 쓰는 변수에는 this를 못 붙이고 멤버필드에만 this를 붙일 수 있다.
 			System.out.print("판매가 : " + this.price + "원");
 			System.out.println("(월 "+monthPrice+"원)");
 		}
