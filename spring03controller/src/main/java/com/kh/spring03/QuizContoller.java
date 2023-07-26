a099fc7b5d8878c0a344b918ecac781509ec78af
wlslals0805 <wlslals0805@naver.com>
2023-07-26 14:53:20 +0900

    최초 컨트롤러 문제 풀이

--- spring03controller/src/main/java/com/kh/spring03/QuizContoller.java
+++ spring03controller/src/main/java/com/kh/spring03/QuizContoller.java
@@ -7,21 +7,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 
-@RestController
+@RestController	//애노테이션(Annotation) - 역할 정의
 public class QuizContoller {
 	
 	Random r = new Random();
 	
 	@RequestMapping("/dice")
-	public int dice(){
+	public String dice(){
 		
-		return r.nextInt(6)+1;
+		int dice= r.nextInt(6)+1;
+		
+		return "주사위: "+dice;
 
 	}
 	@RequestMapping("/lotto")
-	public int lotto() {
+	public String lotto() {
 		
-		return r.nextInt(45)+1;
+		int lotto = r.nextInt(45)+1;
+		
+		return "로또 번호:" + lotto;
 	}
 	
 
