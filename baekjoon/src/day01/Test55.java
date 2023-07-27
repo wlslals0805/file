54e578b7c257783c632dfa661727e0a4032ede8c
wlslals0805 <wlslals0805@naver.com>
2023-07-27 19:38:32 +0900

    백준

--- baekjoon/src/day01/Test55.java
+++ baekjoon/src/day01/Test55.java
@@ -59,6 +59,7 @@ public class Test55 {
 		}
 		System.out.println(count);
 			
+		
 			
 		}
 		
---
+++ baekjoon/src/day01/Test56.java
@@ -0,0 +1,32 @@
+package day01;
+
+public class Test56 {
+
+	public static void main(String[] args) {
+		
+		StringBuilder a = new StringBuilder();
+		StringBuffer b = new StringBuffer();
+		
+		a.append('f');
+		a.append('g');
+		a.append('h');
+		
+		b.append('g');
+		b.append('t');
+		b.append('f');
+		
+		
+		
+		System.out.println(a.toString());
+		
+		System.out.println(a);
+		
+		
+		System.out.println(b);
+		System.out.println(b.toString());
+		
+		System.out.println(b.charAt(0));
+		
+	}
+
+}
