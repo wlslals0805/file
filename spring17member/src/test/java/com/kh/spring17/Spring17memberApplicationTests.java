ca23b38a64b308f85ee6b3a995ef1d63027232f2
wlslals0805 <wlslals0805@naver.com>
2023-10-13 15:52:28 +0900

    spring17 생성

---
+++ spring17member/src/test/java/com/kh/spring17/Spring17memberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.spring17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring17memberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
