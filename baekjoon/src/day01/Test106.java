9d431f87861d261aa8a6ca6ca24c7319e6a878f3
wlslals0805 <wlslals0805@naver.com>
2024-01-29 17:43:57 +0900

    test

---
+++ baekjoon/src/day01/Test106.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Test106 {
+	public String solution(int num) {
+        String answer = "";
+        
+        if(num%2==0){
+            answer = "Even";
+        }
+        else{
+            answer = "Odd";
+        }
+        
+        return answer;
+    }
+
+}
