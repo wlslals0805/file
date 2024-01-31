6b08ab652c7279702cb54ccfb660903d44d9b61b
wlslals0805 <wlslals0805@naver.com>
2024-01-31 21:41:07 +0900

    test

---
+++ baekjoon/src/day01/Test108.java
@@ -0,0 +1,37 @@
+package day01;
+
+public class Test108 {
+	
+	public static void main(String args[]) {
+		
+		String s = "one4seveneight";
+		
+		System.out.println(solution(s));
+		
+	}
+
+	
+	public static int solution(String s) {
+        
+        
+        
+        String[] alp = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        
+        
+        for(int i=0;i<alp.length;i++) {
+        	
+        	String str = alp[i];
+        	
+        	if(s.contains(alp[i])) {
+        		
+        		s = s.replaceAll(str, String.valueOf(i));
+        	}
+        	
+        	
+        }
+        
+        
+        return Integer.parseInt(s);
+    }
+	
+}
