ac9f7e441b7400e3a72945d3f96d623b8141833c
wlslals0805 <wlslals0805@naver.com>
2024-02-12 14:56:34 +0900

    test

---
+++ baekjoon/src/day01/Test120.java
@@ -0,0 +1,33 @@
+package day01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test120 {
+
+	public int solution(int k, int m, int[] score) {
+		int answer = 0;
+
+		Arrays.sort(score);
+
+		int index = score.length - 1;
+
+		int count = score.length / m;
+
+		for (int j = 0; j < count; j++) {
+			List<Integer> list = new ArrayList<>();
+			for (int i = 0; i < m; i++) {
+
+				list.add(score[index]);
+
+				index--;
+
+			}
+
+			answer += m * list.get(list.size() - 1);
+		}
+
+		return answer;
+	}
+}
