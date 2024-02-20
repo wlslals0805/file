0f996470c8e4c9781f1fb45a18b2b23e497764f0
wlslals0805 <wlslals0805@naver.com>
2024-02-20 20:32:26 +0900

    test

--- baekjoon/src/day01/Test122.java
+++ baekjoon/src/day01/Test122.java
@@ -30,5 +30,31 @@ public class Test122 {
 
 		return answer;
 	}
+	
+	public int solution2(int number, int limit, int power) {
+	    int answer = 0;
+
+	    for (int i = 1; i <= number; i++) {
+	        int divisorCount = 0;
+	        
+	        for (int j = 1; j * j <= i; j++) {
+	            if (i % j == 0) {
+	                divisorCount++; // j는 i의 약수
+	                if (j * j != i) { // i/j도 다른 약수
+	                    divisorCount++;
+	                }
+	            }
+	        }
+
+	        if (divisorCount > limit) {
+	            answer += power;
+	        } else {
+	            answer += divisorCount;
+	        }
+	    }
+
+	    return answer;
+	}
+
 
 }
