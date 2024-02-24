bc2e044d59e57d29182b26ebd98068f5161a0b2b
wlslals0805 <wlslals0805@naver.com>
2024-02-24 20:20:18 +0900

    test

--- baekjoon/src/day01/Test115.java
+++ baekjoon/src/day01/Test115.java
@@ -3,6 +3,7 @@ package day01;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Test115 {
 	public List<Integer> solution(int k, int[] score) {
@@ -28,4 +29,29 @@ public class Test115 {
 
 		return answer;
 	}
+	
+	
+
+	public List<Integer> solution2(int k, int[] score) {
+	    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+	    List<Integer> answer = new ArrayList<>();
+
+	    for (int s : score) {
+	        if (minHeap.size() < k) { 
+	            minHeap.add(s);
+	        } else if (s > minHeap.peek()) { 
+	            minHeap.poll();
+	            minHeap.add(s);
+	        }
+	        
+	        if (minHeap.size() == k) {
+	            answer.add(minHeap.peek());
+	        } else {
+	           
+	            answer.add(-1); 
+	        }
+	    }
+	    return answer;
+	}
+
 }
