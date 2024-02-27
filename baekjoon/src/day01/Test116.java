ed5dc232be9bac63b2d7fe89ffb8427b08567351
wlslals0805 <wlslals0805@naver.com>
2024-02-27 18:05:36 +0900

    test

--- baekjoon/src/day01/Test116.java
+++ baekjoon/src/day01/Test116.java
@@ -28,5 +28,27 @@ public class Test116 {
 
 		return answer;
 	}
+	
+	public String solution2(String[] cards1, String[] cards2, String[] goal) {
+	    String answer = "Yes";
+
+	    int indexCards1 = 0;
+	    int indexCards2 = 0;
+
+	    for (int i = 0; i < goal.length; i++) {
+	        String currentGoalCard = goal[i];
+	        
+	        if (indexCards1 < cards1.length && cards1[indexCards1].equals(currentGoalCard)) {
+	            indexCards1++; 
+	        } else if (indexCards2 < cards2.length && cards2[indexCards2].equals(currentGoalCard)) {
+	            indexCards2++; 
+	        } else {
+	            return "No"; 
+	        }
+	    }
+
+	    return answer;
+	}
+
 
 }
