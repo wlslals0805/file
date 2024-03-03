64f82943c0a25665ae849833684e5fc603208e07
wlslals0805 <wlslals0805@naver.com>
2024-03-03 17:32:11 +0900

    test

--- baekjoon/src/day01/Test109.java
+++ baekjoon/src/day01/Test109.java
@@ -22,5 +22,25 @@ public class Test109 {
 	        
 	        return answer;
 	    }
+	 
+	 public String solution2(int[] food) {
+		    StringBuilder answer = new StringBuilder();
+		    
+		    for (int i = 1; i < food.length; i++) {
+		        for (int a = 0; a < food[i] / 2; a++) {
+		            answer.append(i);
+		        }
+		    }
+		    answer.append("0");
+		    
+		    for (int i = food.length - 1; i >= 1; i--) {
+		        for (int a = 0; a < food[i] / 2; a++) {
+		            answer.append(i);
+		        }
+		    }
+		    
+		    return answer.toString();
+		}
+
 
 }
