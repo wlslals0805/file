b1c677a3a9d66ea641453a0a5f229bf6223f633a
wlslals0805 <wlslals0805@naver.com>
2024-03-06 18:12:44 +0900

    test

--- baekjoon/src/day01/Test129.java
+++ baekjoon/src/day01/Test129.java
@@ -5,45 +5,84 @@ import java.util.List;
 
 public class Test129 {
 	public int solution(String dartResult) {
-		int answer = 0;
+        int answer = 0;
+        List<String> strList = new ArrayList<>();
+        char[] ch = dartResult.toCharArray();
+        
+        for(int i=0;i<ch.length;i++){
+            
+            String str = String.valueOf(ch[i]);
+            
+            if(Character.isDigit(ch[i])){
+                
+                if(ch[i+1]=='0'){
+                    strList.add("10");
+                    i=i+1;
+                }
+                else{
+                    strList.add(str);
+                }
+                
+            }
+            else{
+                strList.add(str);
+            }
+            
+        }
+  
+        List<Integer> list = new ArrayList<>();
+        
+      
+        
+        for(int i=0;i<strList.size();i++){
 
-		List<Integer> list = new ArrayList<>();
-
-		char[] ch = dartResult.toCharArray();
-
-		for (int i = 0; i < ch.length; i++) {
-
-			String s = String.valueOf(ch[i]);
-
-			if (Character.isDigit(ch[i])) {
-				System.out.println("s=" + s);
-				if (ch[i + 1] == 'S') {
-					list.add(Integer.parseInt(s));
-					answer += Integer.parseInt(s);
-				} else if (ch[i + 1] == 'D') {
-					list.add(Integer.parseInt(s) * Integer.parseInt(s));
-					answer += Integer.parseInt(s) * Integer.parseInt(s);
-				} else if (ch[i + 1] == 'T') {
-					list.add(Integer.parseInt(s) * Integer.parseInt(s) * Integer.parseInt(s));
-					answer += Integer.parseInt(s) * Integer.parseInt(s) * Integer.parseInt(s);
-				}
-
-			} else if (ch[i] == '*') {
-				answer -= list.get(list.size() - 1);
-				answer += list.get(list.size() - 1) * 2;
-
-				if (i > 3) {
-					answer -= list.get(list.size() - 2);
-					answer += list.get(list.size() - 2) * 2;
-				}
-			} else if (ch[i] == '#') {
-				answer -= list.get(list.size() - 1) * 2;
-
-			}
-			System.out.println(answer);
-
-		}
-
-		return answer;
-	}
+            String s = strList.get(i);
+           
+            if(s.matches("\\d+")){
+                
+                if(strList.get(i+1).equals("S")){
+                    list.add(Integer.parseInt(s));
+                    answer+=Integer.parseInt(s);
+                }
+                else if(strList.get(i+1).equals("D")){
+                    list.add(Integer.parseInt(s)*Integer.parseInt(s));
+                    answer+=Integer.parseInt(s)*Integer.parseInt(s);
+                }
+                else if(strList.get(i+1).equals("T")){
+                    list.add(Integer.parseInt(s)*Integer.parseInt(s)*Integer.parseInt(s));
+                   answer+=Integer.parseInt(s)*Integer.parseInt(s)*Integer.parseInt(s); 
+                }
+    
+            }
+            else if(s.equals("*")){
+                answer-=list.get(list.size()-1);
+                answer+=list.get(list.size()-1)*2;
+                
+                if(i>3){
+                    
+                    if(strList.get(i-3).equals("*")){
+                    answer-=list.get(list.size()-2)*2;
+                    answer+=list.get(list.size()-2)*4;
+                    }
+                    else if(strList.get(i-3).equals("#")){
+                        answer-=list.get(list.size()-2);
+                       
+                    }
+                    else{
+                    answer-=list.get(list.size()-2);
+                    answer+=list.get(list.size()-2)*2;
+                    }
+                }
+            }
+            else if(s.equals("#")){
+                answer-=list.get(list.size()-1)*2;
+                
+            }
+            
+           
+            
+        }
+        
+        return answer;
+    }
 }
