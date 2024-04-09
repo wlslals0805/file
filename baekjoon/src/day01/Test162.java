95f281f0b3503dd2d87b3306b28c150f5c6fb029
wlslals0805 <wlslals0805@naver.com>
2024-04-09 17:31:16 +0900

    test

--- baekjoon/src/day01/Test162.java
+++ baekjoon/src/day01/Test162.java
@@ -2,48 +2,44 @@ package day01;
 
 public class Test162 {
 	
-	//미완성
-	
-	public String solution(String polynomial) {
-        String answer = "";
-        
-        char[] ch = polynomial.toCharArray();
-        
-        for(int j=0;j<ch.length;j++){
-            
-            int count =0;
-            char x = ch[j];
-            
-            if(Character.isDigit(ch[j])){
-                count = Integer.parseInt(String.valueOf(ch[j]))-1;
-                x = ch[j+1];
-            }
-            
-            if(!Character.isDigit(x)
-               &&x!='+'
-               &&!answer.contains(String.valueOf(x))
-              &&x!=' '){
-            for(int i=0;i<ch.length;i++){
-                
-                if(x == ch[i]){
-                    count++;
-                }
-                
-            }
-               
-                System.out.println("x="+x);
-                answer+=String.valueOf(count)+x;
-            }
-            else{
-                answer+=x;
-            }
-            
-            
-           
-            
-        }
-        
-        return answer;
-    }
+	 public String solution(String polynomial) {
+	        String[] terms = polynomial.split(" \\+ ");
+	        int constant = 0;
+	        int xCoefficient = 0;
+
+	        
+	        for (String term : terms) {
+	            if (term.contains("x")) {
+	                
+	                int index = term.indexOf("x");
+	                String coefficient = term.substring(0, index);
+	                if (coefficient.equals("")) {
+	                    xCoefficient += 1;
+	                } else {
+	                    xCoefficient += Integer.parseInt(coefficient);
+	                }
+	            } else {
+	              
+	                constant += Integer.parseInt(term);
+	            }
+	        }
+
+	       
+	        StringBuilder result = new StringBuilder();
+	        if (xCoefficient > 0) {
+	            if (xCoefficient > 1) {
+	                result.append(xCoefficient);
+	            }
+	            result.append("x");
+	            if (constant > 0) {
+	                result.append(" + ");
+	            }
+	        }
+	        if (constant > 0) {
+	            result.append(constant);
+	        }
+
+	        return result.toString();
+	    }
 
 }
