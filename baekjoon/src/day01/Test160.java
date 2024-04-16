55a47eaddeedd25b4cf2780fa4e554ebb517f13f
wlslals0805 <wlslals0805@naver.com>
2024-04-16 15:49:31 +0900

    test

--- baekjoon/src/day01/Test160.java
+++ baekjoon/src/day01/Test160.java
@@ -5,32 +5,31 @@ import java.util.List;
 
 public class Test160 {
 
-	public List<Integer> solution(int[][] score) {
+	
 
-		List<Double> score_list = new ArrayList<>();
-		List<Integer> result = new ArrayList<>();
 
-		for (int i = 0; i < score.length; i++) {
+	    public List<Integer> solution(int[][] score) {
+	        int n = score.length;
+	        List<Double> averages = new ArrayList<>();
+	        List<Integer> ranks = new ArrayList<>();
 
-			score_list.add((score[i][0] + score[i][1]) / (double) 2);
+	        for (int i = 0; i < n; i++) {
+	            double average = (score[i][0] + score[i][1]) / 2.0;
+	            averages.add(average);
+	        }
 
-		}
+	        for (int i = 0; i < n; i++) {
+	            int rank = 1;
+	            for (int j = 0; j < n; j++) {
+	                if (averages.get(i) < averages.get(j)) {
+	                    rank++;
+	                }
+	            }
+	            ranks.add(rank);
+	        }
 
-		for (int i = 0; i < score_list.size(); i++) {
+	        return ranks;
+	    }
+	
 
-			int count = 1;
-			double sc = score_list.get(i);
-
-			for (int j = 0; j < score_list.size(); j++) {
-				if (sc < score_list.get(j)) {
-					count++;
-				}
-			}
-
-			result.add(count);
-
-		}
-
-		return result;
-	}
 }
