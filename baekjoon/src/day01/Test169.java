ec29d0c50a01c9fdf9d6665f91553eba641c5631
wlslals0805 <wlslals0805@naver.com>
2024-04-21 21:32:41 +0900

    test

---
+++ baekjoon/src/day01/Test169.java
@@ -0,0 +1,22 @@
+package day01;
+
+import java.util.Arrays;
+
+public class Test169 {
+
+	public int solution(int[] A, int[] B) {
+		int answer = 0;
+
+		Arrays.sort(A);
+		Arrays.sort(B);
+
+		for (int i = 0; i < A.length; i++) {
+
+			answer += A[i] * B[B.length - i - 1];
+
+		}
+
+		return answer;
+	}
+
+}
